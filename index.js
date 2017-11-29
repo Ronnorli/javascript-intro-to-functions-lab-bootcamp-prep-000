4afcc21c6946a77f67e14a5d42b23cbb08470b00
Ronald Li <ronaldbrooklynli@gmail.com>
2017-11-29 21:36:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === toLowerCase()) {
+  var word = "I can't hear you"
+  if (word.toLowerCase() === toLowerCase()) {
     return "I can't hear you"
   }
 }
